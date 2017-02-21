b00a30876bb2108ca81aef96a5aa8465b181b139
Manny Isles <manny.isles@gmail.com>
2017-02-21 12:33:24 +0800

    Create TaxTest.php

---
+++ tests/TaxTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mannysoft\Tax;
+
+use PHPUnit\Framework\TestCase;
+
+class TaxTest extends TestCase
+{
+
+    public function testBasicReturnsNullOnValidAttempt()
+    {
+        
+    }
+
+    
+}
